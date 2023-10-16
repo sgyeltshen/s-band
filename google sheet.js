4c736eb0b60b8c9febb015c0f2b98471f9fd3d84
sgyeltshen <147610267+sgyeltshen@users.noreply.github.com>
2023-10-16 14:54:18 +0600

    added

--- google sheet.js
+++ google sheet.js
@@ -14,3 +14,22 @@ form.addEventListener("submit", (e) => {
     })
     .catch((error) => console.error("Error!", error.message));
 });
+
+// Get the dropdown and input element by their IDs
+var operationTypeDropdown = document.getElementById("inputState");
+var dataBreakInput = document.getElementById("dataBreakInput");
+
+// Add an onchange event listener to the dropdown
+operationTypeDropdown.addEventListener("change", function () {
+  // Check if the selected option is 'Full PL_PB_1Mbps' or 'Partial FHK_PB'
+  if (
+    operationTypeDropdown.value === "Full PL_PB_1Mbps" ||
+    operationTypeDropdown.value === "Partial FHK_PB"
+  ) {
+    // If the selected option is one of the specified options, disable the input field
+    dataBreakInput.disabled = true;
+  } else {
+    // If the selected option is not one of the specified options, enable the input field
+    dataBreakInput.disabled = false;
+  }
+});
